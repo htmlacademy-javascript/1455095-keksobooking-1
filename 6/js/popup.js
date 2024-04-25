e1d25af81afcdcd9d55134575eb2cef9c976201a
Keks <keks@htmlacademy.ru>
2024-04-25 19:25:01 +0300

    :heavy_check_mark: Сборка #6

--- 6/js/popup.js
+++ 6/js/popup.js
@@ -10,61 +10,24 @@ function gereratePopupFeatures(features) {
 }
 
 function getFilledPostTempalte({ author, offer }) {
+  const { avatar } = author;
+  const { title, address, price, type, guests, rooms, checkin, checkout, features, description, photos } = offer;
+
   return `
     <article class="popup">
-      <img class="popup__avatar ${author.avatar ? '' : 'hidden'}" src="${author.avatar ? author.avatar : ''}" width="70" height="70" alt="Аватар пользователя">
-      <h3 class="popup__title ${offer.title ? '' : 'hidden'}">${offer.title ? offer.title : ''}</h3>
-      <p class="popup__text popup__text--address ${offer.address ? '' : 'hidden'}">${offer.address ? offer.address : ''}</p>
-      <p class="popup__text popup__text--price ${offer.price ? '' : 'hidden'}">${offer.price ? offer.price : ''}<span>₽/ночь</span></p>
-      <h4 class="popup__type ${offer.type ? '' : 'hidden'}">${offer.type ? Data.TYPE_ARRAY_DECODING[offer.type] : ''}</h4>
-
-      <p class="popup__text popup__text--capacity ${offer.guests && offer.rooms ? '' : 'hidden'}">${offer.rooms ? offer.rooms : ''} комнаты для ${offer.guests ? offer.guests : ''} гостей</p>
-      <p class="popup__text popup__text--time ${offer.checkin && offer.checkout ? '' : 'hidden'}">Заезд после ${offer.checkin ? offer.checkin : ''}, выезд до ${offer.checkout ? offer.checkout : ''}</p>
-      <ul class="popup__features">${gereratePopupFeatures(offer.features)}</ul>
-      <p class="popup__description ${offer.description ? '' : 'hidden'}">${offer.description ? offer.description : ''}</p>
-      <div class="popup__photos">${offer.photos.map((url) =>`<img src="${url}" class="popup__photo" width="45" height="40" alt="Фотография жилья">`).join('')}</div>
+      <img class="popup__avatar ${avatar ? '' : 'hidden'}" src="${avatar ? avatar : ''}" width="70" height="70" alt="Аватар пользователя">
+      <h3 class="popup__title ${title ? '' : 'hidden'}">${title ? title : ''}</h3>
+      <p class="popup__text popup__text--address ${address ? '' : 'hidden'}">${address ? address : ''}</p>
+      <p class="popup__text popup__text--price ${price ? '' : 'hidden'}">${price ? price : ''}<span>₽/ночь</span></p>
+      <h4 class="popup__type ${type ? '' : 'hidden'}">${type ? Data.TYPE_ARRAY_DECODING[type] : ''}</h4>
+      <p class="popup__text popup__text--capacity ${guests && rooms ? '' : 'hidden'}">${rooms ? rooms : ''} комнаты для ${guests ? guests : ''} гостей</p>
+      <p class="popup__text popup__text--time ${checkin && checkout ? '' : 'hidden'}">Заезд после ${checkin ? checkin : ''}, выезд до ${checkout ? checkout : ''}</p>
+      <ul class="popup__features">${gereratePopupFeatures(features)}</ul>
+      <p class="popup__description ${description ? '' : 'hidden'}">${description ? description : ''}</p>
+      <div class="popup__photos">${photos.map((url) => `<img src="${url}" class="popup__photo" width="45" height="40" alt="Фотография жилья">`).join('')}</div>
     </article>
   `;
 }
-
-// //  Вариант с функциями
-
-// function isValueEmpty(value){
-//   return !value;
-// }
-
-// function getHideClass(...args) {
-//   if(args.some(isValueEmpty)){
-//     return 'hidden';
-//   } else {
-//     return '';
-//   }
-// }
-
-// function getContent(value){
-//   if (!(isValueEmpty(value))) {
-//     return value;
-//   } else {
-//     return '';
-//   }
-// }
-
-// function getFilledPostTempalte({ author, offer }) {
-//   return `
-//     <article class="popup">
-//       <img src="${getContent(author.avatar)}" class="popup__avatar ${getHideClass(author.avatar)}" width="70" height="70" alt="Аватар пользователя">
-//       <h3 class="popup__title ${getHideClass(offer.title)}">${getContent(offer.title)}</h3>
-//       <p class="popup__text popup__text--address ${getHideClass(offer.address)}">${getContent(offer.address)}</p>
-//       <p class="popup__text popup__text--price ${getHideClass(offer.price)}">${getContent(offer.price)} <span>₽/ночь</span></p>
-//       <h4 class="popup__type ${getHideClass(offer.type)}">${offer.type ? Data.TYPE_ARRAY_DECODING[offer.type] : ''}</h4>
-//       <p class="popup__text popup__text--capacity ${getHideClass(offer.rooms, offer.guests)}">${getContent(offer.rooms)} комнаты для ${getContent(offer.guests)} гостей</p>
-//       <p class="popup__text popup__text--time ${getHideClass(offer.checkin, offer.checkout)}">Заезд после ${getContent(offer.checkin)}, выезд до ${getContent(offer.checkout)}</p>
-//       <ul class="popup__features">${gereratePopupFeatures(offer.features)}</ul>
-//       <p class="popup__description ${getHideClass(offer.description)}">${getContent(offer.description)}</p>
-//       <div class="popup__photos">${offer.photos.map((url) =>`<img src="${url}" class="popup__photo" width="45" height="40" alt="Фотография жилья">`).join('')}</div>
-//     </article>
-//   `;
-// }
 
 function createPost(post) {
   const postTemplate = getFilledPostTempalte(post);
