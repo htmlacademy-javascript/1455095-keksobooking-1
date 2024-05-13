dc0f5595de3f58c1d801791db5617543f00b60f0
Keks <keks@htmlacademy.ru>
2024-05-13 21:58:19 +0300

    :heavy_check_mark: Сборка #8

---
+++ 8/js/functions.js
@@ -0,0 +1,66 @@
+function checkStr (string) {
+  const strConverted = string.replaceAll(' ', '').toLowerCase();
+  const strReversed = string.replaceAll(' ', '').toLowerCase().split('').reverse().join('');
+
+  return strConverted === strReversed;
+}
+
+checkStr('ДовОд');
+
+function extractNum (param) {
+  let extractedNums = '';
+  const arr = param.toString().split('');
+
+  for (let i = 0; i < arr.length; i++) {
+    if (!isNaN(parseInt((arr[i]), 10))) {
+      extractedNums = extractedNums + (arr[i]);
+    }
+  }
+
+  if (extractedNums === ''){
+    return NaN;
+  }
+
+  return parseInt(extractedNums, 10);
+}
+
+extractNum('2023 год');
+
+function getFullStr(initStr, lengthStr, sumbols){
+  if (initStr.length > lengthStr) {
+    return initStr;
+  }
+
+  if (initStr.length < lengthStr){
+    const lackSumbols = lengthStr - initStr.length;
+    let needSymbols = '';
+
+    while (needSymbols.length < lackSumbols) {
+      needSymbols = needSymbols + sumbols;
+    }
+
+    needSymbols = needSymbols.slice(0, lackSumbols);
+
+    return needSymbols + initStr;
+  }
+}
+
+getFullStr('qwerty', 4, '0');
+
+function getInt1(minInt, maxInt, countInt = 0){
+  if (minInt < 0 || maxInt < 0) {
+    return NaN;
+  }
+
+  if (maxInt < minInt) {
+    const saveInt = maxInt;
+    maxInt = minInt;
+    minInt = saveInt;
+  }
+
+  const floatInt = Math.random() * (maxInt - minInt) + minInt;
+
+  return parseFloat(floatInt.toFixed(countInt));
+}
+
+getInt1(1, 2);
